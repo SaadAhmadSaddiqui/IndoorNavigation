69fee7ddab3ec3fdd3b92289d9b884316252c372
SaadAhmadSaddiqui <mehunsaadi@gmail.com>
2020-06-06 12:03:01 +0400

    Re-Added Location Selector
    Hopefully all the junk is gone.

---
+++ Assets/Location Selector/Scripts/LocationSelector.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class LocationSelector : MonoBehaviour
+{
+    MeshCollider planeCollider;
+    public Vector3 worldPosition;
+    public GameObject cube;
+    public Text posText;
+    Ray ray;
+
+    private Camera cam;
+    
+
+    private void Start()
+    {
+        planeCollider = GetComponent<MeshCollider>();
+        cam = Camera.main;
+
+    }
+
+    void Update()
+    {
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitData;
+
+        if (planeCollider.Raycast(ray, out hitData, 1000))
+        {
+            worldPosition = hitData.point;
+        }
+        if (Input.GetMouseButtonDown(0) && !IsMouseOverUI())
+        { 
+            if (GameObject.Find("Cube(Clone)"))
+            {
+                Destroy(GameObject.Find("Cube(Clone)"));
+            }
+            Instantiate(cube, worldPosition, Quaternion.identity);
+            posText.text = "X Position: " + worldPosition.x + "\nZ Position: " + worldPosition.z; 
+        }
+    }
+
+    bool IsMouseOverUI()
+    {
+        return EventSystem.current.IsPointerOverGameObject();
+    }
+}
---
+++ Assets/Location Selector/Scripts/LocationSelectorUIHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationSelectorUIHandler : MonoBehaviour
+{
+    public GameObject controlsTint;
+    public GameObject controlsPanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            HideControlsPanel();
+        }
+    }
+
+    public void ShowControlsPanel()
+    {
+        controlsPanel.SetActive(true);
+        controlsTint.SetActive(true);
+    }
+
+    private void HideControlsPanel()
+    {
+        controlsPanel.SetActive(false);
+        controlsTint.SetActive(false);
+    }
+}
